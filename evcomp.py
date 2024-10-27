cdc96c117b36b18d86785e9db9cd8b1dc854df06
nishifwd <155698899+nishifwd@users.noreply.github.com>
2024-10-27 11:40:58 +0800

    Update evcomp.py

--- evcomp.py
+++ evcomp.py
@@ -131,4 +131,8 @@ def main(POP_SIZE, MUT_RATE, TARGET, GENES):
       st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
       generation+=1
 
+if st.button("Calculate"):
+    main(POP_SIZE, MUT_RATE, TARGET, GENES)
+
 result = main(POP_SIZE, MUT_RATE, TARGET, GENES)
+
