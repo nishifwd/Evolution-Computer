9bc8d03aa2eb3736e8bdcbcfaeaf2768d39edf8b
nishifwd <155698899+nishifwd@users.noreply.github.com>
2024-10-27 11:21:49 +0800

    Update evcomp.py

--- evcomp.py
+++ evcomp.py
@@ -15,7 +15,7 @@ MUT_RATE = 0.2
 
 #TARGET: Our goal.
 TARGET = 'danish'
-
+TARGET = st.text_input("Enter your name")
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
 
