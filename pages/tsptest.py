603e171d63c1b04039fe5996023e395d7029ac8e
nishifwd <155698899+nishifwd@users.noreply.github.com>
2024-11-05 05:58:35 +0800

    Update tsptest.py

--- pages/tsptest.py
+++ pages/tsptest.py
@@ -56,9 +56,7 @@ for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
             ax.plot([city_x, other_x], [city_y, other_y], color='gray', linestyle='-', linewidth=1, alpha=0.1)
 
 fig.set_size_inches(16, 12)
-
-if st.button("Submit"):
-    st.pyplot(fig)
+st.pyplot(fig)
 
 
 #population
