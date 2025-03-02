f6af2bb96863f6728a7a8ca503ff208aa50c69f5
nishifwd <155698899+nishifwd@users.noreply.github.com>
2025-03-02 12:35:31 +0800

    Update sign_language.py

--- pages/sign_language.py
+++ pages/sign_language.py
@@ -23,7 +23,7 @@ uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "png", "jpeg
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True
     
     img_array = preprocess_image(image)
     prediction = model.predict(img_array)
